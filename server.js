9152bdf36c09ed370ae90064f3ab005034d57cfc
Fahmi Ihza Sulistya Ananta <fahmiihza@gmail.com>
2022-11-19 11:01:39 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const { urlencoded } = require('express')
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const port = 3000
+
+const user = []
+
+app.set('view-engine','ejs')
+app.use(express.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    res.render('index.ejs')
+})
+
+app.post('/login', function (req, res) {
+  
+})
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10)
+      users.push({
+        id: Date.now().toString(),
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      })
+      res.redirect('/login')
+    } catch {
+      res.redirect('/register')
+    }
+  })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
